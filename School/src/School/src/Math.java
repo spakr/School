994fb511d45f9303413255016240ba84e14dd257
spakr <43635221+spakr@users.noreply.github.com>
2019-09-16 13:26:19 +0200

    Add files via upload

---
+++ School/src/School/src/Math.java
@@ -0,0 +1,12 @@
+public class Math extends Subject {
+    public static void main(String[] args) {
+        int num = 1526, reversed = 0;
+        while(num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        System.out.println("Reversed Number: " + reversed);
+    }
+}
+
